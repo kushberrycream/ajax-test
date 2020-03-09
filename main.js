6edfc4e254d429b2f63384c78e7acee36badc51b
kushberrycream <tom@wilson-express.co.uk>
2020-03-09 12:39:20 +0000

    implement pagination

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const baseURL = "https://swapi.co/api/"
-
-function getData(type, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
 
     xhr.onreadystatechange = function () {
@@ -16,27 +14,41 @@ function getData(type, cb) {
 function getTableHeaders(obj) {
     var tableHeaders = [];
 
-    Object.keys(obj).forEach(function(key) {
+    Object.keys(obj).forEach(function (key) {
         tableHeaders.push(`<td>${key}</td>`)
     });
 
     return `<tr>${tableHeaders}</tr>`;
 };
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+        <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    };
+};
+
+function writeToDocument(url) {
     var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
 
-    getData(type, function (data) {
-        console.dir(data);
+    getData(url, function (data) {
+        var pagination;
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        };
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function (item) {
             var dataRow = [];
 
-            Object.keys(item).forEach(function(key) {
+            Object.keys(item).forEach(function (key) {
                 var rowData = item[key].toString();
                 var truncatedData = rowData.substring(0, 15);
                 dataRow.push(`<td>${truncatedData}</td>`);
@@ -44,6 +56,6 @@ function writeToDocument(type) {
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 };
